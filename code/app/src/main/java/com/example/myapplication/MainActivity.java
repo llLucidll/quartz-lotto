6942ea0e748f796c1f9e7c06529f4a8fe8d651a6
llLucidll <akhil10831@gmail.com>
2024-11-05 18:16:48 -0700

    Fixing Weird gitIgnore

--- code/app/src/main/java/com/example/myapplication/MainActivity.java
+++ code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         NotificationUtils.createNotificationChannel(this); //Creating channel for notifications
 
