54d1f1810009a92ecb27fd454846b61524ac24ff
llLucidll <akhil10831@gmail.com>
2024-11-26 19:35:21 -0700

    Added Organizer Profile View

--- code/app/src/main/java/com/example/myapplication/Views/EntrantEventDetailsView.java
+++ code/app/src/main/java/com/example/myapplication/Views/EntrantEventDetailsView.java
@@ -1,4 +1,7 @@
 package com.example.myapplication.Views;
 
+/*
+Event Screen that shows the details of the event to a potential attendee.
+ */
 public class EntrantEventDetailsView {
 }
--- code/app/src/main/java/com/example/myapplication/Views/NotificationGroupView.java
+++ code/app/src/main/java/com/example/myapplication/Views/NotificationGroupView.java
@@ -1,4 +1,8 @@
 package com.example.myapplication.Views;
 
+
+/*
+Notification Group to manually send notifications to users
+ */
 public class NotificationGroupView {
 }
