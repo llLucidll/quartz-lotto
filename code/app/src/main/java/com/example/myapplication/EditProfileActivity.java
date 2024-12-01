7661825828bc84e55d17e15a44dbbacc3b6acd81
llLucidll <akhil10831@gmail.com>
2024-11-30 21:54:07 -0700

    Fixing the Godforsaken crash that has been plaguing us for the past hour you all are welcome

--- code/app/src/main/java/com/example/myapplication/EditProfileActivity.java
+++ code/app/src/main/java/com/example/myapplication/EditProfileActivity.java
@@ -230,7 +230,7 @@ public class EditProfileActivity extends BaseActivity {
         profileData.put("isAdmin", isAdmin);
         profileData.put("notificationsPerm", notificationsPerm);
         profileData.put("events", null);
-        profileData.put("eventsAttending", new HashMap<>());
+        //profileData.put("eventsAttending", new HashMap<>());
 
         DocumentReference userRef = db.collection("users").document(deviceId);
         userRef.set(profileData)
