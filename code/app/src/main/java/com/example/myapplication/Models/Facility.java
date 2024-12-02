68637cdd7cb241071f4922b48d7bf14ba1128079
Ayra Qutub <ayra4027@gmail.com>
2024-12-02 15:16:43 -0700

    did more javadocs

--- code/app/src/main/java/com/example/myapplication/Models/Facility.java
+++ code/app/src/main/java/com/example/myapplication/Models/Facility.java
@@ -22,33 +22,74 @@ public class Facility {
         this.id = id;
     }
 
+    /**
+     * Default constructor required for Firebase deserialization.
+     * @return The URL of the facility's image.
+     */
+
     public String getImageUrl() {
         return ImageUrl;
     }
+    /**
+     * Sets the URL of the facility's image.
+     * @param imageUrl The URL of the facility's image.
+     */
+
     public void setImageUrl(String imageUrl) {
         this.ImageUrl = imageUrl;
     }
+
+    /**
+     * Retrieves the location of the facility.
+     * @return The location of the facility.
+     */
 
 
     public String getLocation() {
         return location;
     }
+
+    /**
+     * Sets the location of the facility.
+     * @param location The location of the facility.
+     */
+
     public void setLocation(String location) {
         this.location = location;
     }
+
+    /**
+     * Retrieves the name of the facility.
+     * @return The name of the facility.
+     */
 
 
     public String getName() {
         return name;
     }
+
+    /**
+     * Sets the name of the facility.
+     * @param name The name of the facility.
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * Retrieves the unique identifier for the facility.
+     * @return The unique identifier for the facility.
+     */
 
 
     public String getId() {
         return id;
     }
+
+    /**
+     * Sets the unique identifier for the facility.
+     * @param id The unique identifier for the facility.
+     */
     public void setId(String id) {
         this.id = id;
     }
